ca7a47a2c0b03074edc53090eebcdbf26c955784
ncyra <157491443+ncyra@users.noreply.github.com>
2024-06-05 00:53:19 +0800

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-1.png" alt="">
+         <h3>our email</h3>
+         <a href="grocmart@gmail.com">grocmart@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/fbb.png" alt="">
+         <h3>Our Social</h3>
+         <p>https://www.fb.com/profile</p>
+      </div>
+
+      <div class="box">
+         <img src="images/location.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Polangui, Albay - 4503</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">09457632891</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>GrocMart</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/Fruit Bowl.gif" alt="">
+</div>
