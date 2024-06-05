1dae1d61a43e7a90a5ed00d5100952b49ca60cf4
ncyra <157491443+ncyra@users.noreply.github.com>
2024-06-05 14:05:57 +0800

    Add files via upload

--- search.php
+++ search.php
@@ -18,95 +18,102 @@ include 'components/add_cart.php';
 <html lang="en">
 <head>
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>search page</title>
+   <title>Search Page</title>
 
-   <!-- font awesome cdn link  -->
+   <!-- Font Awesome CDN Link -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
+   <!-- Custom CSS File Link -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
-<!-- header section starts  -->
+
+<!-- Header Section Starts -->
 <?php include 'components/user_header.php'; ?>
-<!-- header section ends -->
+<!-- Header Section Ends -->
 
-<!-- search form section starts  -->
-
+<!-- Search Form Section Starts -->
 <section class="search-form">
    <form method="post" action="">
-      <input type="text" name="search_box" placeholder="search here..." class="box">
+      <input type="text" name="search_box" placeholder="Search here..." class="box" required>
       <button type="submit" name="search_btn" class="fas fa-search"></button>
    </form>
 </section>
+<!-- Search Form Section Ends -->
 
-<!-- search form section ends -->
-
-
+<!-- Products Section Starts -->
 <section class="products" style="min-height: 100vh; padding-top:0;">
-
 <div class="box-container">
 
-      <?php
-         if(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
-         $search_box = $_POST['search_box'];
-         $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'");
-         $select_products->execute();
-         if($select_products->rowCount() > 0){
-            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
-      ?>
-      <form action="" method="post" class="box">
-         <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
-         <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-         <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
-         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
-         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
-         <div class="name"><?= $fetch_products['name']; ?></div>
-         <div class="flex">
-            <div class="price"><span>$</span><?= $fetch_products['price']; ?></div>
-            <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
-         </div>
-      </form>
-      <?php
+   <?php
+   if(isset($_POST['search_box']) || isset($_POST['search_btn'])){
+      $search_box = htmlspecialchars($_POST['search_box']);
+      $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE ? OR description LIKE ? OR category LIKE ?");
+      $select_products->execute(['%' . $search_box . '%', '%' . $search_box . '%', '%' . $search_box . '%']);
+      
+      if($select_products->rowCount() > 0){
+         while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <form action="" method="post" class="box">
+      <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+      <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
+      <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
+      <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
+      <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+      <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+      <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+      <div class="name"><?= $fetch_products['name']; ?></div>
+      <div class="description"><?= $fetch_products['description']; ?></div>
+      <div class="category">Category: <?= $fetch_products['category']; ?></div>
+      <div class="flex">
+         <div class="price"><span>₱</span><?= $fetch_products['price']; ?></div>
+         <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+      </div>
+   </form>
+   <?php
+         }
+      }else{
+         // If no products found, display relevant items based on categorie
+         $related_products = $conn->prepare("SELECT * FROM `products` WHERE category IN (SELECT category FROM `products` WHERE name LIKE ? OR description LIKE ?) ORDER BY RAND() LIMIT 6");
+         $related_products->execute(['%' . $search_box . '%', '%' . $search_box . '%']);
+         if($related_products->rowCount() > 0){
+            while($fetch_related = $related_products->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <form action="" method="post" class="box">
+      <input type="hidden" name="pid" value="<?= $fetch_related['id']; ?>">
+      <input type="hidden" name="name" value="<?= $fetch_related['name']; ?>">
+      <input type="hidden" name="price" value="<?= $fetch_related['price']; ?>">
+      <input type="hidden" name="image" value="<?= $fetch_related['image']; ?>">
+      <a href="quick_view.php?pid=<?= $fetch_related['id']; ?>" class="fas fa-eye"></a>
+      <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+      <img src="uploaded_img/<?= $fetch_related['image']; ?>" alt="">
+      <div class="name"><?= $fetch_related['name']; ?></div>
+      <div class="description"><?= $fetch_related['description']; ?></div>
+      <div class="category">Category: <?= $fetch_related['category']; ?></div>
+      <div class="flex">
+         <div class="price"><span>₱</span><?= $fetch_related['price']; ?></div>
+         <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+      </div>
+   </form>
+   <?php
             }
-         }else{
-            echo '<p class="empty">no products added yet!</p>';
+         } else {
+            echo '<p class="empty">No relevant items found!</p>';
          }
       }
-      ?>
+   }
+   ?>
 
-   </div>
-
+</div>
 </section>
+<!-- Products Section Ends -->
 
-
-
-
-
-
-
-
-
-
-
-<!-- footer section starts  -->
+<!-- Footer Section Starts -->
 <?php include 'components/footer.php'; ?>
-<!-- footer section ends -->
+<!-- Footer Section Ends -->
 
-
-
-
-
-
-
-<!-- custom js file link  -->
+<!-- Custom JS File Link -->
 <script src="js/script.js"></script>
 
 </body>
